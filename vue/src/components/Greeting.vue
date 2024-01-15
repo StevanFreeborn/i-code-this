8a14a095522e5809c84ee47172cf931fd2259053
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-15 10:42:20 -0600

    chore: setup eslint and prettier

---
+++ vue/src/components/Greeting.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Check out
+    <a href="https://icodethis.com/" target="_blank">iCodeThis</a>, for all the
+    challenges
+  </p>
+</template>
+
+<style scoped></style>
